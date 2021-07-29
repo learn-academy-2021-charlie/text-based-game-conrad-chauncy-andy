a58078f03a4ac0a5425b65bf2a7979fef9affae7
CSapien08 <sapiano08@gmail.com>
2021-07-29 11:59:21 -0700

    cleaned up file

--- story.rb
+++ story.rb
@@ -4,38 +4,78 @@
 def game_start
     "Welcome to Western Treasure Hunt, please enter your name.".each_char do |value|
         putc value 
-        sleep 0.05
+        #sleep 0.05
     end
 puts
 gets.chomp
 end
 
 name = game_start
-p name
+
 def intro name
     "Hello #{name} you're about to start your journey, before you there are 3 roads one on the left going towards the river, one on the right going towards a cave and one in the center going towards the ocean... will you go left right or center?".each_char do |value|
             putc value
-            sleep 0.05
+            #sleep 0.05
         end
 puts
 gets.chomp
 end
 
-intro_path = intro (name)
+#return a string from intro(name) with left, right, or center
+intro_path = intro(name)
 
-def option_left 
-    "You have reached the river.. it is a rocky river with a fast current.. you want to cross the river and you have three options to do so option 1) a rickety bridge option2) an old boat option3) swim across please type your option number bellow ".each_char do |value|
+def option_left_story
+    "You have reached the river.. it is a rocky river with a fast current.. you want to cross the river and you have three options to do so option 1) a rickety bridge option 2) an old boat option 3) swim across please type your option number bellow ".each_char do |value|
         putc value 
-        sleep 0.05
+        #sleep 0.05
     end
 puts
-gets.chomp
 end
+
+def option_left_number
+    gets.chomp
+end    
 
 def option_bridge
     "The bridge collapsed as you were walking on it and you drowned under a pile of wood GAME OVER!".each_char do |value|
         putc value 
         sleep 0.05
+    end
+end
+
+def option_boat
+    "You made to the end, but then a tiger ate you! GAME OVER!".each_char do |value|
+        putc value 
+        sleep 0.05
+    end
+end
+
+def option_center 
+    "You have reached the cave.. it is a dark and smelly cave .. you have a ravine in front of you you have to cross you have three options 1) A hanging vine you can swing across option 2) full send jump option 3) climb down into the ravine and try to walk across ".each_char do |value|
+        putc value 
+        #sleep 0.05
+    end
+puts
+gets.chomp
+end
+
+def option_right 
+    "You have reached the ocean.. waves are crashing hard on the beach against the sand and the skies are cloudy.. ahead of you is an old capsized barge to reach it you have three options option 1) Just swim to it.. option 2) an old fishermans kayak option3) sit and think about more ways to get to the barge ".each_char do |value|
+        putc value 
+        #sleep 0.05
+    end
+puts
+gets.chomp
+end
+
+
+def  start_path string
+    if string.downcase == 'left'
+        option_left_story
+    elsif string.downcase == 'center'
+        option_center
+    elsif string.downcase == 'right'
+        option_right
     end
 end
 
@@ -49,38 +89,10 @@ def  left_options number
     end
 end
 
+start_path(intro_path)
 
+option_left = left_options(option_left_story)
 
-def option_center 
-    "You have reached the cave.. it is a dark and smelly cave .. you have a ravine in front of you you have to cross you have three options 1) A hanging vine you can swing across option 2) full send jump option 3) climb down into the ravine and try to walk across ".each_char do |value|
-        putc value 
-        sleep 0.05
-    end
-puts
-gets.chomp
-end
-
-def option_right 
-    "You have reached the ocean.. waves are crashing hard on the beach against the sand and the skies are cloudy.. ahead of you is an old capsized barge to reach it you have three options option 1) Just swim to it.. option 2) an old fishermans kayak option3) sit and think about more ways to get to the barge ".each_char do |value|
-        putc value 
-        sleep 0.05
-    end
-puts
-gets.chomp
-end
-
-
-def  start_path string
-    if string.downcase == 'left'
-        option_left
-    elsif string.downcase == 'center'
-        option_center
-    elsif string.downcase == 'right'
-        option_right
-    end
-end
-
-path = start_path intro_path
 
 
 
